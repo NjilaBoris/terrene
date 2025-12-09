8dca0413f2595888caf18bdd8df29556ab1d828d
NjilaBoris <njilaboris2@gmail.com>
2025-12-09 19:40:36 +0100

    Implement Studio, Blueprints

--- src/app/blueprints/page.jsx
+++ src/app/blueprints/page.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Nav from "@/components/Nav/Nav";
+import Gallery from "@/components/Gallery/Gallery";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Nav />
+      <div className="page blueprints">
+        <Gallery />
+      </div>
+    </>
+  );
 };
 
 export default page;
